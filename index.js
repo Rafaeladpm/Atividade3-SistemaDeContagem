fbbc7d1497cab282061da62be1a29470c6a08f7c
Rafaela de Paula Mendes <rafaeladepaulamendes@gmail.com>
2022-08-19 15:37:56 -0300

    SistemaDeContagem

---
+++ index.js
@@ -0,0 +1,10 @@
+let listadeacademia = ["João","Sofia","Carlos","Vivianne","Sarah"]
+
+for (let index = 0; index < ListadeAlunos.length; index++) {
+    console.log (index)
+    if (index ==0){
+        console.log ("zero localizado:" + ListadeAlunos [index])
+    } else if (index % 2) {console.log ("impar:" + ListadeAlunos [index])}
+    else [console.log ("par:" + ListadeAlunos [index])]
+    console.log [ListadeAlunos]
+}
